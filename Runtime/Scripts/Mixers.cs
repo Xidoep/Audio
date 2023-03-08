883a2b4d90947ab7b9842ddca48cdf80b11107c1
xeviegido <Xidostudio@gmail.com>
2023-03-08 17:12:21 +0100

    savable

--- Runtime/Scripts/Mixers.cs
+++ Runtime/Scripts/Mixers.cs
@@ -7,27 +7,24 @@ using XS_Utils;
 [CreateAssetMenu(menuName = "Xido Studio/Audio/Mixers", fileName = "Mixers")]
 public class Mixers : ScriptableObject 
 {
-    public readonly string KEY_MASTER = "VolumMaster";
-    public readonly string KEY_MUSICA = "VolumMusica";
-    public readonly string KEY_SO = "VolumSons";
-    public readonly string KEY_REVERB = "Reverberacio";
-    public readonly string KEY_ECO = "Eco";
-    public readonly string KEY_LOWPASSGENERAL = "LowPassGeneral";
-    public readonly string KEY_LOWPASSMUSICA = "LowPassMusica";
+    const string KEY_MASTER = "VolumMaster";
+    const string KEY_MUSICA = "VolumMusica";
+    const string KEY_SO = "VolumSons";
+    const string KEY_REVERB = "Reverberacio";
+    const string KEY_ECO = "Eco";
+    const string KEY_LOWPASSGENERAL = "LowPassGeneral";
+    const string KEY_LOWPASSMUSICA = "LowPassMusica";
 
     public static Mixers Instance;
-
-    [SerializeField] Guardat guardat;
 
     [SerializeField] AudioMixer mixer;
     [SerializeField] AudioMixerGroup master;
     [SerializeField] AudioMixerGroup musica;
     [SerializeField] AudioMixerGroup sons;
 
-    [Range(0, 1.25f)] [SerializeField] float volumMaster = 1;
-    [Range(0, 1.25f)] [SerializeField] float volumMusica = 1;
-    [Range(0, 1.25f)] [SerializeField] float volumSons = 1;
-
+    [SerializeField] SavableVariable<float> volumMaster;
+    [SerializeField] SavableVariable<float> volumMusica;
+    [SerializeField] SavableVariable<float> volumSo;
     [Range(0, 1)] [SerializeField] float reverberacio = 0;
     [Range(0, 1)] [SerializeField] float eco = 0;
     [Range(0, 1)] [SerializeField] float lowpassGeneral = 0;
@@ -35,129 +32,103 @@ public class Mixers : ScriptableObject
 
     public AudioMixerGroup Sons => sons;
 
-    //public bool MixerActualitzat => Mixers.Instance.GetFloat(Mixers.Instance.master, KEY_MASTER) == Mixers.Instance.volumMaster;
 
     private void OnEnable()
     {
-        Debugar.Log("[Mixers] OnEnable() => Instance, Carregar(), Actualitzar() i guardat.onLoad += Carregar");
+        Debugar.Log("[Mixers] OnEnable() => Instance, Actualitzar()");
         Instance = this;
-        Carregar();
-        //Actualitzar();
-        guardat.onLoad += Carregar;
+        Actualitzar();
     }
 
-    private void OnDisable()
-    {
-        guardat.onLoad -= Carregar;
-    }
-
-    public void Carregar()
-    {
-        SetMaster((float)guardat.Get(KEY_MASTER, 1f));
-        SetMusica((float)guardat.Get(KEY_MUSICA, 1f));
-        SetSo((float)guardat.Get(KEY_SO, 1f));
-        SetReverberacio((float)guardat.Get(KEY_REVERB, 0));
-        SetEco((float)guardat.Get(KEY_ECO, 0));
-        SetLowPassGeneral((float)guardat.Get(KEY_LOWPASSGENERAL, 0));
-        SetLowPassMusica((float)guardat.Get(KEY_LOWPASSMUSICA, 0));
-
-        //Actualitzar();
-    }
+   
 
     //SETTERS
     public void SetMaster(float _volum)
     {
-        volumMaster = _volum;
-        SetFloat(master, KEY_MASTER, -((volumMaster - 1) * -80));
-        guardat.SetLocal(KEY_MASTER, volumMaster);
+        volumMaster.Valor = _volum;
+        SetMaster();
     }
     public void SetMusica(float _volum)
     {
-        volumMusica = _volum;
-        SetFloat(musica, KEY_MUSICA, -((volumMusica - 1) * -80));
-        guardat.SetLocal(KEY_MUSICA, volumMusica);
+        volumMusica.Valor = _volum;
+        SetMusica();
     }
     public void SetSo(float _volum)
     {
-        volumSons = _volum;
-        SetFloat(sons, KEY_SO, -((volumSons - 1) * -80));
-        guardat.SetLocal(KEY_SO, volumSons);
+        volumMaster.Valor = _volum;
+        SetSo();
     }
-
     public void SetReverberacio(float _valor)
     {
         reverberacio = _valor;
-        SetFloat(master, KEY_REVERB, -10000 + (10000 * _valor));
-        guardat.SetLocal(KEY_REVERB, reverberacio);
+        SetReverberacio();
     }
     public void SetEco(float _valor)
     {
         eco = _valor;
-        SetFloat(sons, KEY_ECO, _valor / 20f);
-        guardat.SetLocal(KEY_ECO, eco);
+        SetEco();
     }
-
     public void SetLowPassGeneral(float _valor)
     {
         lowpassGeneral = _valor;
-        SetFloat(master, KEY_LOWPASSGENERAL, 21990 - (21990 * _valor) + 10);
-        guardat.SetLocal(KEY_LOWPASSGENERAL, lowpassGeneral);
+        SetLowPassGeneral();
     }
-
     public void SetLowPassMusica(float _valor)
     {
         lowpassMusica = _valor;
-        SetFloat(musica, KEY_LOWPASSMUSICA, 21990 - (21990 * _valor) + 10);
-        guardat.SetLocal(KEY_LOWPASSMUSICA, lowpassMusica);
+        SetLowPassMusica();
     }
 
+
+
+
+    void SetMaster() => SetFloat(master, KEY_MASTER, ToVolum(volumMaster.Valor));
+    void SetMusica() => SetFloat(master, KEY_MUSICA, ToVolum(volumMaster.Valor));
+    void SetSo() => SetFloat(sons, KEY_SO, ToVolum(volumSo.Valor));
+    void SetReverberacio() => SetFloat(master, KEY_REVERB, ToReverberacio(reverberacio));
+    void SetEco() => SetFloat(sons, KEY_ECO, ToEco(eco));
+    void SetLowPassGeneral() => SetFloat(master, KEY_LOWPASSGENERAL, ToLowPass(lowpassGeneral));
+    void SetLowPassMusica() => SetFloat(musica, KEY_LOWPASSMUSICA, ToLowPass(lowpassMusica));
     void SetFloat(AudioMixerGroup _mixer, string _key, float _valor) => _mixer.audioMixer.SetFloat(_key, _valor);
 
 
 
-    //GETTERS
-    //float
-    float GetReverberacio()
-    {
-        master.audioMixer.GetFloat(KEY_REVERB, out float _tmp);
-        return (1 + (_tmp / 10000f));
-    }
-    float GetEco()
-    {
-        sons.audioMixer.GetFloat(KEY_ECO, out float _tmp);
-        return _tmp * 20f;
-    }
-    float GetLowPass(AudioMixerGroup _mixer, string _key)
-    {
-        _mixer.audioMixer.GetFloat(_key, out float _tmp);
-        return 1 - (_tmp / 22000f);
-    }
-    float GetFloat(AudioMixerGroup _mixer, string _key)
-    {
-        _mixer.audioMixer.GetFloat(_key, out float _tmp);
-        return (_tmp / 80f) + 1;
-    }
 
-    private void OnValidate()
-    {
-        guardat = XS_Editor.LoadGuardat<Guardat>();
-        Debugar.Log("[Mixer] OnValidate()");
-        Actualitzar();
-        if (Application.isPlaying)
-        {
-            
-        }
-    }
 
     public void Actualitzar()
     {
-        SetMaster(volumMaster);
-        SetMusica(volumMusica);
-        SetSo(volumSons);
+        SetMaster();
+        SetMusica();
+        SetSo();
 
-        if (GetReverberacio() != reverberacio) SetReverberacio(reverberacio);
-        if (GetEco() != eco) SetEco(eco);
-        if (GetLowPass(master, KEY_LOWPASSGENERAL) != lowpassGeneral) SetLowPassGeneral(lowpassGeneral);
-        if (GetLowPass(musica, KEY_LOWPASSMUSICA) != lowpassMusica) SetLowPassMusica(lowpassMusica);
+        SetReverberacio();
+        SetEco();
+        SetLowPassGeneral();
+        SetLowPassMusica();
+    }
+
+
+
+
+    float ToVolum(float volum) => -((volum - 1) * -80);
+    float ToReverberacio(float volum) => -10000 + (10000 * volum);
+    float ToEco(float volum) => volum / 20f;
+    float ToLowPass(float volum) => 21990 - (21990 * volum) + 10;
+
+
+
+
+
+
+    private void OnValidate()
+    {
+        //guardat = XS_Editor.LoadGuardat<Guardat>();
+        Debugar.Log("[Mixer] OnValidate()");
+
+        volumMaster = new SavableVariable<float>(KEY_MASTER, Guardat.Direccio.Local, 1);
+        volumMusica = new SavableVariable<float>(KEY_MUSICA, Guardat.Direccio.Local, 1);
+        volumSo = new SavableVariable<float>(KEY_SO, Guardat.Direccio.Local, 1);
+
+        Actualitzar();
     }
 }
